67a529e12d2a8ca58cf589889429796506cd6558
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-07-20 22:59:52 +0300

    #dev fix config provider

---
+++ src/ConfigProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace SELN\App;
+
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+
+        ];
+    }
+
+}
