0d503ae8c923408f6ca677fea141036e07f1beeb
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-07-21 23:46:52 +0300

    #5 use factory

--- src/Core/ConfigProvider.php
+++ src/Core/ConfigProvider.php
@@ -9,12 +9,19 @@ declare(strict_types=1);
 namespace SELN\App\Core;
 
 
+use Doctrine\ORM\EntityManagerInterface;
+use SELN\App\Core\Doctrine\EntityManagerFactory;
+
 class ConfigProvider
 {
     public function __invoke(): array
     {
         return [
-
+            'dependencies' => [
+                'factories' => [
+                    EntityManagerInterface::class => EntityManagerFactory::class
+                ]
+            ]
         ];
     }
 
