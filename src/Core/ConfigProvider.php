b8a3d8e0c8a180689f41ddf0dea075b2658252e8
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-07-21 16:14:36 +0300

    #5 install cors middleware

---
+++ src/Core/ConfigProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace SELN\App\Core;
+
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+
+        ];
+    }
+
+}
