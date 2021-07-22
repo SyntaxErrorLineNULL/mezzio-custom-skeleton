91bd862882edb2c6511a47291363d3bfcd05dc4c
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-07-22 17:12:52 +0300

    #dev create user doctrine repository

---
+++ src/Application/Infrastructure/Repository/DoctrineUserRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace SELN\App\Application\Infrastructure\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\ORMException;
+use SELN\App\Application\Domain\Entity\User;
+use SELN\App\Application\Domain\Repository\UserRepository;
+
+class DoctrineUserRepository extends EntityRepository implements UserRepository
+{
+
+    /**
+     * @throws ORMException
+     */
+    public function create(User $user): void
+    {
+        $this->_em->persist($user);
+    }
+
+    /**
+     * @throws ORMException
+     */
+    public function remove(User $user): void
+    {
+        $this->_em->remove($user);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function getById(string $id): User
+    {
+        /** @var User|null $user */
+        $user = $this->find($id);
+        if ($user === null) {
+            throw new \Exception('Entity not found');
+        }
+        return $user;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAll(): array
+    {
+        $this->findAll();
+    }
+
+    /**
+     * @param string $email
+     * @return User|null
+     */
+    public function findByEmail(string $email): ?User
+    {
+        $this->findOneBy(['email' => $email]);
+    }
+}
