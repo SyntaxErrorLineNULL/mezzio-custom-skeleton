14b5a5e8ee3cad8c4d836bd1157bc2905af0e8a3
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-07-22 15:36:10 +0300

    #dev create translator config

---
+++ config/translator.global.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+return [
+    'translator' => [
+        'lang' => 'ru',
+        'resources' => [
+            [
+                'xlf',
+                __DIR__ . '../vendor/symfony/validator/Resources/translations/validators.ru.xlf',
+                'ru',
+                'validators',
+            ],
+            [
+                'xlf',
+                __DIR__ . '../vendor/symfony/validator/Resources/translations/validators.en.xlf',
+                'en',
+                'validators',
+            ]
+        ],
+    ],
+];
