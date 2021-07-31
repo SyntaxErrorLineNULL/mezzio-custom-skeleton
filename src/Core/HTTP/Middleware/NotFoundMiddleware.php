fe673a5ae5aca77ffa1f0ef69d6d46c40ea49517
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-07-31 23:16:02 +0300

    #11 create not found middleware

---
+++ src/Core/HTTP/Middleware/NotFoundMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace SELN\App\Core\HTTP\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class NotFoundMiddleware implements MiddlewareInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        // TODO: Implement process() method.
+    }
+}
