60200a16298b89d5280bba5eaa1028f25d02a0dd
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-01 19:37:11 +0300

    #14 update request builder

--- src/Core/HTTP/Request/RequestSchema.php
+++ src/Core/HTTP/Request/RequestSchema.php
@@ -7,10 +7,13 @@ declare(strict_types=1);
 
 namespace SELN\App\Core\HTTP\Request;
 
+use Psr\Http\Message\ServerRequestInterface;
 use SELN\App\Core\HTTP\RequestValidator\RequestValidator;
+use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-    use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Serializer;
 
 class RequestSchema
 {
@@ -25,9 +28,14 @@ class RequestSchema
     public function __construct(RequestValidator $requestValidator)
     {
         $this->serializer = new Serializer(
-            [new ObjectNormalizer()],
+            [new ObjectNormalizer(null, null, null, new ReflectionExtractor()), new ArrayDenormalizer()],
             ['json' => new JsonEncoder()]
         );
         $this->requestValidator = $requestValidator;
+    }
+
+    public function getRequestProperty(string $schema, ServerRequestInterface $request)
+    {
+        $schema = $this->serializer->deserialize($request->getBody(), $schema, self::TYPE);
     }
 }
