90c6fe4c29dd631e87ebcccf1b874e5657009478
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-01 16:00:02 +0300

    #13 RequestValidatorException update

--- src/Core/HTTP/RequestValidator/RequestValidatorException.php
+++ src/Core/HTTP/RequestValidator/RequestValidatorException.php
@@ -7,7 +7,24 @@ declare(strict_types=1);
 
 namespace SELN\App\Core\HTTP\RequestValidator;
 
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+
 class RequestValidatorException extends \Exception
 {
+    private ConstraintViolationListInterface $violations;
 
+    public function __construct(
+        ConstraintViolationListInterface $violations,
+        string $message = 'Invalid request',
+        int $code = 0,
+        \Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+        $this->violations = $violations;
+    }
+
+    public function getViolations(): ConstraintViolationListInterface
+    {
+        return $this->violations;
+    }
 }
