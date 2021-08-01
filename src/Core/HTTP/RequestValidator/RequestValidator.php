45ddc35716ac30ffba8c87f60018751a019a5aeb
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-01 15:46:47 +0300

    #13 RequestValidator update

--- src/Core/HTTP/RequestValidator/RequestValidator.php
+++ src/Core/HTTP/RequestValidator/RequestValidator.php
@@ -7,7 +7,21 @@ declare(strict_types=1);
 
 namespace SELN\App\Core\HTTP\RequestValidator;
 
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
 class RequestValidator
 {
 
+    public function __construct(private ValidatorInterface $validator) {}
+
+    /**
+     * @throws RequestValidatorException
+     */
+    public function validate(object $object): void
+    {
+        $violations = $this->validator->validate($object);
+        if ($violations->count() !== 0) {
+            throw new RequestValidatorException($violations);
+        }
+    }
 }
