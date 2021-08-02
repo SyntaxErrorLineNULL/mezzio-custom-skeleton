86ea745ff2bc22681f4d4213a90466396d9c3909
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-02 16:30:42 +0300

    #9 update Api config provider

--- src/Api/ConfigProvider.php
+++ src/Api/ConfigProvider.php
@@ -8,12 +8,19 @@ declare(strict_types=1);
 
 namespace SELN\App\Api;
 
+use SELN\App\Core\Authentication\JWTService;
+use SELN\App\Core\Factory\JWTServiceFactory;
+
 class ConfigProvider
 {
     public function __invoke(): array
     {
         return [
-
+            'dependencies' => [
+                'factories' => [
+                    JWTService::class => JWTServiceFactory::class,
+                ]
+            ]
         ];
     }
 }
