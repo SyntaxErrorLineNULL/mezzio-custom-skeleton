98c184a8c52d4bade08a4a76b3d583970d709c6b
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-03 17:42:58 +0300

    #9 update jwt factory

--- src/Core/Factory/JWTServiceFactory.php
+++ src/Core/Factory/JWTServiceFactory.php
@@ -24,6 +24,6 @@ class JWTServiceFactory implements FactoryInterface
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null): JWTService
     {
         $config = $container->get('config')['jwt'];
-        return new JWTService($config['secret'], $config['algorithm']);
+        return new JWTService($config['secret']);
     }
 }
