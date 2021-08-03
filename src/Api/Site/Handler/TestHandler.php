b79a859a98445894663820e970ab5d6dbcb06e12
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-03 18:33:31 +0300

    #9 test handler and schema

--- src/Api/Site/Handler/TestHandler.php
+++ src/Api/Site/Handler/TestHandler.php
@@ -13,12 +13,32 @@ use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use SELN\App\Application\Domain\Repository\UserRepository;
+use SELN\App\Application\Service\PasswordService;
+use SELN\App\Core\Authentication\JWTService;
+use SELN\App\Core\HTTP\Request\RequestSchema;
+use SELN\App\Core\HTTP\RequestValidator\RequestValidatorException;
 
 class TestHandler implements RequestHandlerInterface
 {
 
+    public function __construct(
+        private JWTService $service,
+        private UserRepository $userRepository,
+        private PasswordService $passwordService,
+        private RequestSchema $requestSchema
+    )
+    {
+    }
+
+    /**
+     * @throws RequestValidatorException
+     */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        return new JsonResponse('hello');
+        /** @var TestSchema $schema */
+        $schema = $this->requestSchema->getRequestProperty(TestSchema::class, $request);
+
+        return new JsonResponse($schema->name);
     }
 }
---
+++ src/Api/Site/Handler/TestSchema.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace SELN\App\Api\Site\Handler;
+
+class TestSchema
+{
+    public string $name;
+}
