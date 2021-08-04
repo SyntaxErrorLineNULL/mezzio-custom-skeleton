0b638782d2cd98373f248bc4e9fee3a621bba7db
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-04 16:12:19 +0300

    #9 init mail service

---
+++ src/Core/Service/Mail/MailService.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace SELN\App\Core\Service\Mail;
+
+class MailService
+{
+    public function send(string $email, string $message)
+    {
+
+    }
+}
