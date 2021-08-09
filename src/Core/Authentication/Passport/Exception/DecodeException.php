35073ecf138939aebded638490e1888f3e1a1b5e
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-09 16:36:07 +0300

    #15 create exception

---
+++ src/Core/Authentication/Passport/Exception/DecodeException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace SELN\App\Core\Authentication\Passport\Exception;
+
+class DecodeException extends \LogicException
+{
+
+}
