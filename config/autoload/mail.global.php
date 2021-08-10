31efe91cb0a447edfdde5038d9da58c796afe65b
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-10 16:17:15 +0300

    #9 mailer config

---
+++ config/autoload/mail.global.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+return [
+    'mailer' => [
+        'host' => getenv('MAILER_HOST'),
+        'port' => getenv('MAILER_PORT'),
+        'user' => getenv('MAILER_USERNAME'),
+        'password' => getenv('MAILER_PASSWORD'),
+        'encryption' => getenv('MAILER_ENCRYPTION'),
+        'from' => getenv('MAILER_FROM_EMAIL')
+    ]
+];
