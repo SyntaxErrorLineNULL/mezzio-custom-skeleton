5063ada98855d83c8ad45041796cd21ff78ddebb
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-11 23:53:39 +0300

    #9 mail send exception

--- src/Core/Service/Mail/MailService.php
+++ src/Core/Service/Mail/MailService.php
@@ -7,24 +7,24 @@ declare(strict_types=1);
 
 namespace SELN\App\Core\Service\Mail;
 
-use Symfony\Component\Mailer\Mailer;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
+
+use Swift_Mailer;
+use Swift_Message;
+use Swift_RfcComplianceException;
 
 class MailService
 {
-
-    public function __construct(
-        private \Swift_Mailer $mailer
-    ) {}
+    public function __construct(private Swift_Mailer $mailer) {}
 
     public function send(string $email, string $body): void
     {
-        $message = (new \Swift_Message())
+        $message = (new Swift_Message())
             ->setFrom('cyberorange16@gmail.com')
             ->setTo($email)
-            ->setBody($body)
-        ;
-        $this->mailer->send($message);
+            ->setBody($body);
+
+        if ($this->mailer->send($message) === 0) {
+            throw new MailSendException("Unable to send email to this ${$email} email address.");
+        }
     }
 }
