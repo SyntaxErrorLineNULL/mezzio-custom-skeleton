a58f229b3b35859dd121a509e9d12640e710a0e7
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-11 23:23:40 +0300

    #9 mail service

--- src/Core/Service/Mail/MailService.php
+++ src/Core/Service/Mail/MailService.php
@@ -7,10 +7,24 @@ declare(strict_types=1);
 
 namespace SELN\App\Core\Service\Mail;
 
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
 class MailService
 {
-    public function send(string $email, string $message)
-    {
 
+    public function __construct(
+        private \Swift_Mailer $mailer
+    ) {}
+
+    public function send(string $email, string $message): void
+    {
+        $message = (new \Swift_Message())
+            ->setFrom()
+            ->setTo()
+            ->setBody('hello')
+        ;
+        $this->mailer->send();
     }
 }
