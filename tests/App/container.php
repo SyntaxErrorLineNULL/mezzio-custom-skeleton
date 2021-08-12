8a66c43c56a1f30cf71fa04914ed02f3ec2c7d31
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-12 15:38:51 +0300

    app container for test fix

--- tests/App/container.php
+++ tests/App/container.php
@@ -4,3 +4,20 @@
  */
 
 declare(strict_types=1);
+
+use Laminas\ConfigAggregator\ArrayProvider,
+    Laminas\ConfigAggregator\ConfigAggregator,
+    Laminas\ServiceManager\ServiceManager;
+
+$appConfig = require __DIR__ . '/../../config/config.php';
+
+$configAggregator = new ConfigAggregator([
+    new ArrayProvider($appConfig)
+]);
+
+$config = $configAggregator->getMergedConfig();
+
+$dependencies = $config['dependencies'];
+$dependencies['services']['config'] = $config;
+
+return new ServiceManager($dependencies);
