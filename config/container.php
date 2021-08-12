c512e057a68aeac2752505de9535237b1aa641c8
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-12 15:38:25 +0300

    fix

--- config/container.php
+++ config/container.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Laminas\ServiceManager\ServiceManager;
 
-$config  = require realpath(__DIR__) . '/config.php';
+$config = require realpath(__DIR__) . '/config.php';
 $dependencies = $config['dependencies'];
 $dependencies['services']['config'] = $config;
 
