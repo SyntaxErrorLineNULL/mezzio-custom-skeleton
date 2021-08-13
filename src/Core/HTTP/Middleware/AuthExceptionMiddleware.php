425838b1ca0911a679809848e98edd50015811e0
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-13 23:55:08 +0300

    update auth exception middleware

--- src/Core/HTTP/Middleware/AuthExceptionMiddleware.php
+++ src/Core/HTTP/Middleware/AuthExceptionMiddleware.php
@@ -7,19 +7,28 @@ declare(strict_types=1);
 
 namespace SELN\App\Core\HTTP\Middleware;
 
+use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use SELN\App\Core\Authentication\Passport\Exception\DecodeException;
 
 class AuthExceptionMiddleware implements MiddlewareInterface
 {
-
     /**
-     * @inheritDoc
+     * @param ServerRequestInterface $request
+     * @param RequestHandlerInterface $handler
+     * @return ResponseInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-
+        try {
+            return $handler->handle($request);
+        } catch (DecodeException $exception) {
+            return new JsonResponse([
+                'message' => $exception->getMessage()
+            ], 401);
+        }
     }
 }
