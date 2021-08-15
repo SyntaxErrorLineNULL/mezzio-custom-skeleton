8c00d496aa5e3f41355a345dc339b3d2be1822fa
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-15 21:53:00 +0300

    init Auth middleware exception test

---
+++ tests/unit/Core/Middleware/AuthExceptionMiddlewareTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace SELN\App\Test\UnitTest\Core\Middleware;
+
+use Codeception\Test\Unit;
+
+class AuthExceptionMiddlewareTest extends Unit
+{
+
+}
