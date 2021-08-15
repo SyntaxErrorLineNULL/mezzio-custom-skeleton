4439689512a4744ae0b5ff83bfabb457fbf753d6
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-15 23:19:47 +0300

    update validator middleware test

--- tests/unit/Core/Middleware/ValidationExceptionMiddlewareTest.php
+++ tests/unit/Core/Middleware/ValidationExceptionMiddlewareTest.php
@@ -8,6 +8,10 @@ declare(strict_types=1);
 namespace SELN\App\Test\UnitTest\Core\Middleware;
 
 use Codeception\Test\Unit;
+use Laminas\Diactoros\ResponseFactory;
+use Laminas\Diactoros\ServerRequestFactory;
+use Psr\Http\Server\RequestHandlerInterface;
+use SELN\App\Core\HTTP\Middleware\ValidationExceptionMiddleware;
 use SELN\App\Test\UnitTest\Core\UnitTemplate;
 
 class ValidationExceptionMiddlewareTest extends Unit implements UnitTemplate
@@ -15,7 +19,14 @@ class ValidationExceptionMiddlewareTest extends Unit implements UnitTemplate
 
     public function testSuccess(): void
     {
+        $middleware = new ValidationExceptionMiddleware();
+        $handler = $this->createStub(RequestHandlerInterface::class);
+        $handler->method('handle')->willReturn($source = (new ResponseFactory())->createResponse());
 
+        $request = (new ServerRequestFactory())->createServerRequest('POST', 'http://localhost');
+        $response = $middleware->process($request, $handler);
+
+        $this->assertEquals($source, $response);
     }
 
     public function testException(): void
