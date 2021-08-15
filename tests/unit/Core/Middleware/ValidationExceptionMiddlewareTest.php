beb53cc1bd12a9023c0c8a5a5034721704b6ac11
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-15 23:32:30 +0300

    update validator middleware test

--- tests/unit/Core/Middleware/ValidationExceptionMiddlewareTest.php
+++ tests/unit/Core/Middleware/ValidationExceptionMiddlewareTest.php
@@ -12,7 +12,10 @@ use Laminas\Diactoros\ResponseFactory;
 use Laminas\Diactoros\ServerRequestFactory;
 use Psr\Http\Server\RequestHandlerInterface;
 use SELN\App\Core\HTTP\Middleware\ValidationExceptionMiddleware;
+use SELN\App\Core\HTTP\RequestValidator\RequestValidatorException;
 use SELN\App\Test\UnitTest\Core\UnitTemplate;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
 
 class ValidationExceptionMiddlewareTest extends Unit implements UnitTemplate
 {
@@ -31,6 +34,29 @@ class ValidationExceptionMiddlewareTest extends Unit implements UnitTemplate
 
     public function testException(): void
     {
-        // TODO: Implement testException() method.
+        $middleware = new ValidationExceptionMiddleware();
+
+        $violations = new ConstraintViolationList([
+            new ConstraintViolation('Incorrect Email', null, [], null, 'email', 'not-email'),
+            new ConstraintViolation('Empty Password', null, [], null, 'password', ''),
+        ]);
+
+        $handler = $this->createStub(RequestHandlerInterface::class);
+        $handler->method('handle')->willThrowException(new RequestValidatorException($violations));
+
+        $request = (new ServerRequestFactory())->createServerRequest('POST', 'http://localhost');
+        $response = $middleware->process($request, $handler);
+
+        $this->assertEquals(422, $response->getStatusCode());
+        $this->assertJson($body = (string)$response->getBody());
+
+        $message = json_decode($body, true);
+
+        $this->assertEquals([
+            'message' => [
+                'email' => 'Incorrect Email',
+                'password' => 'Empty Password'
+            ]
+        ], $message);
     }
 }
