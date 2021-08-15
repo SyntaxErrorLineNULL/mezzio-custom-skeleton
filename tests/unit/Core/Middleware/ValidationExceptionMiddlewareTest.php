3a87e642655d7f7cd35c2e613595bea1085ff580
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-15 23:18:51 +0300

    create validator middleware test

---
+++ tests/unit/Core/Middleware/ValidationExceptionMiddlewareTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Author: SyntaxErrorLineNULL.
+ */
+
+declare(strict_types=1);
+
+namespace SELN\App\Test\UnitTest\Core\Middleware;
+
+use Codeception\Test\Unit;
+use SELN\App\Test\UnitTest\Core\UnitTemplate;
+
+class ValidationExceptionMiddlewareTest extends Unit implements UnitTemplate
+{
+
+    public function testSuccess(): void
+    {
+
+    }
+
+    public function testException(): void
+    {
+        // TODO: Implement testException() method.
+    }
+}
