be9e4b3b1cc46b48a4a59c57148df89a8a315c1b
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-15 22:00:43 +0300

    update Auth middleware exception test

--- tests/unit/Core/Middleware/AuthExceptionMiddlewareTest.php
+++ tests/unit/Core/Middleware/AuthExceptionMiddlewareTest.php
@@ -8,8 +8,27 @@ declare(strict_types=1);
 namespace SELN\App\Test\UnitTest\Core\Middleware;
 
 use Codeception\Test\Unit;
+use Laminas\Diactoros\ResponseFactory;
+use Laminas\Diactoros\ServerRequestFactory;
+use Psr\Http\Server\RequestHandlerInterface;
+use SELN\App\Core\HTTP\Middleware\AuthExceptionMiddleware;
 
 class AuthExceptionMiddlewareTest extends Unit
 {
+    public function testSuccess(): void
+    {
+        $middleware = new AuthExceptionMiddleware();
+        $handler = $this->createStub(RequestHandlerInterface::class);
+        $handler->method('handle')->willReturn($source = (new ResponseFactory())->createResponse());
 
+        $request = (new ServerRequestFactory())->createServerRequest('POST', 'http://localhost');
+        $response = $middleware->process($request, $handler);
+
+        $this->assertEquals($source, $response);
+    }
+
+    public function testException(): void
+    {
+
+    }
 }
